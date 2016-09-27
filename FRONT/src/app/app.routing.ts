68cee090249d58a95e3af0cbc6fecf798d9098b4
Paweł Zawodny <pawel.zawodny@gmail.com>
2016-09-27 11:40:53 +0200

    Update app.routing.ts

--- FRONT/src/app/app.routing.ts
+++ FRONT/src/app/app.routing.ts
@@ -18,4 +18,4 @@ export const appRoutingProviders: any[] = [
 
 ];
 
-export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes, { useHash: true });
